458b119750800de10afc25fbbe7147b2e390926e
parikhaniruddh <30910920+parikhaniruddh@users.noreply.github.com>
2025-09-04 15:33:19 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pickle
 import numpy as np
 
-import joblib
-
-model = joblib.load("/content/linear_regression_model.pkl")
+# Load the saved model
+model = pickle.load(open('linear_reg.sav', 'rb'))
 
 st.title('Sales Prediction App')
 
@@ -17,5 +16,4 @@ Newspaper = st.number_input('Newspaper Advertising Budget', min_value=0.0)
 if st.button('Predict Sales'):
     input_data = np.array([[TV, Radio, Newspaper]])
     prediction = model.predict(input_data)[0]
-
     st.success(f'Predicted Sales: {prediction:.2f}')
